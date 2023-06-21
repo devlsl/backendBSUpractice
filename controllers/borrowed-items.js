58bae1bfbe23482bba23687388af27ac21e5816c
DrejPalych <98765863+Andrey-Pavlovich-Kalmykov@users.noreply.github.com>
2023-06-21 15:33:44 +0300

    Update borrowed-items.js

--- controllers/borrowed-items.js
+++ controllers/borrowed-items.js
@@ -1,1 +1,23 @@
+const db = require('../db')
+const noSpaces = require('../utils/noSpaces')
 
+class BorrowedItemsController {
+  async getItems(req, res) {
+    try {
+      const response = await db.query('select * from borrowed_items')
+
+      const items = response.rows.map((item) => ({
+        id: item['Инвентарный номер'],
+        name: noSpaces(item['Название'])
+      }))
+
+      res.json({
+        items: items
+      })
+    } catch (error) {
+      res.json(error.message)
+    }
+  }
+}
+
+module.exports = new BorrowedItemsController()
